04d745fd4b51c85c030897653f341222903b72bf
Ronal Sanchez <88067203+sronaal@users.noreply.github.com>
2021-11-26 14:37:02 -0500

    Reto-01

---
+++ emigrant/Controllers/EmigrantesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using emigrant.Data;
+using emigrant.Models;
+
+namespace emigrant.Controllers
+{
+    public class EmigrantesController : Controller
+    {
+        private emigrantContext db = new emigrantContext();
+
+        // GET: Emigrantes
+        public ActionResult Index()
+        {
+            return View(db.Emigrantes.ToList());
+        }
+
+        // GET: Emigrantes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Emigrante emigrante = db.Emigrantes.Find(id);
+            if (emigrante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emigrante);
+        }
+
+        // GET: Emigrantes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Emigrantes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,apellidos,tipoIdentificacion,numeroIdentificacion,pais,fechaNacimiento,correoElectronico,numeroTelefonico,direccionActual,ciudad,situacionLaboral")] Emigrante emigrante)
+        {
+            if (ModelState.IsValid)
+            {   
+                
+
+                db.Emigrantes.Add(emigrante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            
+
+            return View(emigrante);
+        }
+
+        // GET: Emigrantes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Emigrante emigrante = db.Emigrantes.Find(id);
+            if (emigrante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emigrante);
+        }
+
+        // POST: Emigrantes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,apellidos,tipoIdentificacion,numeroIdentificacion,pais,fechaNacimiento,correoElectronico,numeroTelefonico,direccionActual,ciudad,situacionLaboral")] Emigrante emigrante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(emigrante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emigrante);
+        }
+
+        // GET: Emigrantes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Emigrante emigrante = db.Emigrantes.Find(id);
+            if (emigrante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(emigrante);
+        }
+
+        // POST: Emigrantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Emigrante emigrante = db.Emigrantes.Find(id);
+            db.Emigrantes.Remove(emigrante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ emigrant/Models/EmigranteDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+namespace emigrant.Models
+{
+    public class EmigranteDB
+    {
+
+        private ConexionDB conexion = new ConexionDB();
+
+        private SqlCommand comando = new SqlCommand();
+
+        private SqlDataAdapter leer;
+
+        public int registrarMigrante(Emigrante migrante)
+        {
+            comando.Connection = conexion.abrirConexion();
+
+
+            return 0;
+        }
+    }
+}
