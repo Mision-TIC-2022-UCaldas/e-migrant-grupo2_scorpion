864579b541f73ba713636275bb86f1336a0fcaa1
sronaal <88067203+sronaal@users.noreply.github.com>
2021-11-26 21:00:22 -0500

    Modelo

---
+++ emigrant/Models/Novedades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Concurrent;
+using System.ComponentModel.DataAnnotations;
+namespace emigrant.Models
+{
+    public class Novedades
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public int diasActivad { get; set; }
+
+        [Required]
+        public int TextoExplicativo { get; set; }
+    }
+}
