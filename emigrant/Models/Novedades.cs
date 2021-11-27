e2242d13f379b543378df615500508cbbdd33717
BrayanOvalle16 <brayanovalle2@outlook.com>
2021-11-27 12:55:25 -0500

    HU-26

--- emigrant/Models/Novedades.cs
+++ emigrant/Models/Novedades.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Collections.Concurrent;
-using System.ComponentModel.DataAnnotations;
-namespace emigrant.Models
+﻿namespace emigrant.Models
 {
     public class Novedades
     {
-        [Key]
-        public int id { get; set; }
-
-        [Required]
-        public int diasActivad { get; set; }
-
-        [Required]
-        public int TextoExplicativo { get; set; }
     }
 }
